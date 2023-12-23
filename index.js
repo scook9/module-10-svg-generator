d2c83e55400411f55b1fb463db455701735ad967
[Spencer Cook] <spencerrcook@hotmail.com>
2023-12-23 12:04:50 -0700

    initial file structure and pseudo code

---
+++ index.js
@@ -0,0 +1,12 @@
+//file should take user input and return an SVG file with their logo
+
+//inquirer for user input
+//3 characters
+//text color, keyword or hex
+//shape: circle, triangle, or square
+//fs to write file to 'logo.svg'
+//300x200 px image
+
+//import js file for shape classes
+//jest for unit tests
+//video submission
