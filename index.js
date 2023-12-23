2fad7f29f6800e350692f57469946185afd674ca
[Spencer Cook] <spencerrcook@hotmail.com>
2023-12-23 13:22:53 -0700

    added test svg and circle class

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const shapes = require("./lib/shapes.js");
 
 //inquirer for user input
 
 //3 characters
-//text color, keyword or hex
-//shape: circle, triangle, or square
+//text color, keyword or hex - can color be passed as a variable equal to its name/string?
+//shape: circle, triangle, or square - need classes
 //fs to write file to 'logo.svg'
 //300x200 px image
 
@@ -26,7 +27,26 @@ function userPrompt(questions) {
   // response is an object with keys equal to the names in the questions array
   inquirer
     .prompt(questions)
-    .then((response) => writeToFile("./examples/logo.svg", TBD));
+    .then((response) =>
+      writeToFile(
+        "./examples/logo.svg",
+        generateShape(response.color, response.shape, response.text)
+      )
+    );
+}
+
+//checks which shape class to use, returns the SVG string to render
+function generateShape(color, shape, text) {
+  if (shape === "Triangle") {
+    const generatedShape = new shapes.Triangle(color, text);
+    return generatedShape.render();
+  } else if (shape === "Circle") {
+    const generatedShape = new shapes.Circle(color, text);
+    return generatedShape.render();
+  } else if (shape === "Square") {
+    const generatedShape = new shapes.Square(color, text);
+    return generatedShape.render();
+  }
 }
 
 function writeToFile(fileName, data) {
